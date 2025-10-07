ed13cf49eac17694bf6ec3ff4a5945c5fc45a143
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-10-07 09:54:01 +0530

    feat: createProduct controller

---
+++ apps/product/src/controllers/product.controller.ts
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import { Request, Response } from "express";
+import { Product } from "../model/product.model";
+import { createProductSchema, updateProductSchema } from "../types";
+import { uploadImage } from "../service/imagekit.service";
+
+interface Filter {
+  $text?: { $search: string };
+  ["price.amount"]?: {
+    $gte?: number;
+    $lte?: number;
+  };
+}
+
+interface MulterFile extends Express.Multer.File {}
+
+// - `POST /` - Create product (seller/admin only)
+export const createProduct = async (req: Request, res: Response) => {
+  try {
+    const parser = createProductSchema.safeParse(req.body);
+    if (!parser.success)
+      return res.status(400).json({
+        message: "Invalid data",
+        error: JSON.parse(parser.error.message).map(
+          (m: any) => `${m.path}: ${m.message}`
+        ),
+      });
+
+      const files = (req.files as MulterFile[]) || []
+
+    const images = await Promise.all(
+      files.map((file) => uploadImage({ buffer: file.buffer }))
+    );
+
+    const product = await Product.create({
+      title: parser.data.title,
+      description: parser.data.description,
+      price: parser.data.price,
+      seller: req.sellerId,
+      images,
+      stock: parser.data.stock,
+    });
+
+    return res.status(200).json({ message: "Product created", product });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to create product" });
+  }
+};
+

8c7cef7a88b434e837a51aafd5143d8e7928de93
fazalkadivar21 <fazalkadivar21@gmail.com>
2025-10-07 09:56:42 +0530

    feat: added delete Product controller

--- apps/product/src/controllers/product.controller.ts
+++ apps/product/src/controllers/product.controller.ts
@@ -145,3 +145,23 @@ export const updateProduct = async (req: Request, res: Response) => {
   }
 };
 
+// - `DELETE /:id` - Delete product (seller only)
+export const deleteProduct = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id as string))
+      return res.status(400).json({ message: "Invalid id." });
+
+    const deletedProduct = await Product.findByIdAndDelete(id);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ message: "Product not found." });
+    }
+
+    return res.status(200).json({ message: "Product deleted." });
+  } catch (error) {
+    return res.status(500).json({ message: "Failed to delete product" });
+  }
+};
+
+
